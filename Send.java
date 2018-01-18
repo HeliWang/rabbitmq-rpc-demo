25065b6f574c1bf96916f045a625eee21cdd09cd
harlan <h379wang@uwaterloo.ca>
2018-01-17 21:38:16 -0500

    some draft

---
+++ Send.java
@@ -0,0 +1,27 @@
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+public class Send {
+
+  private final static String QUEUE_NAME = "hello";
+
+  public static void main(String[] argv) throws Exception {
+    ConnectionFactory factory = new ConnectionFactory();
+    factory.setHost("white-swan.rmq.cloudamqp.com");
+    factory.setVirtualHost("rxuhxbbh");
+    factory.setUsername("rxuhxbbh");
+    factory.setPassword("mkywerCtgEVDC-LOARLwgi7mm4xhteZA");
+    Connection connection = factory.newConnection();
+    Channel channel = connection.createChannel();
+
+    channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+    String message = "Hello World!";
+    channel.basicPublish("", QUEUE_NAME, null, message.getBytes("UTF-8"));
+    System.out.println(" [x] Sent '" + message + "'");
+
+    channel.close();
+    connection.close();
+  }
+}
