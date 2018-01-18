af729982346b1af6a9be6907777240fd61bde035
harlan <h379wang@uwaterloo.ca>
2018-01-18 00:01:03 -0500

    mature

--- python/fib.py
+++ python/fib.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 import pika
 import json
+import sys
 
 connection = pika.BlockingConnection(pika.URLParameters("amqp://rxuhxbbh:mkywerCtgEVDC-LOARLwgi7mm4xhteZA@white-swan.rmq.cloudamqp.com/rxuhxbbh"))
 channel = connection.channel()
-channel.queue_declare(queue='rpc_queue')
 
 def fib(n):
     if n == 0:
@@ -14,25 +14,15 @@ def fib(n):
     else:
         return fib(n-1) + fib(n-2)
 
-def on_request(ch, method, props, body):
-    print(ch)
-    body = json.loads(body)
+def publish_back():
+    body = json.load(sys.stdin)
     n = int(body['n'])
-
     print(" [.] fib(%s)" % n)
-
     response = fib(n)
-    ch.basic_publish(exchange='',
+    channel.basic_publish(exchange='',
                      routing_key=props.reply_to,
                      properties=pika.BasicProperties(correlation_id=props.correlation_id),
                      body=str(response))
-    ch.basic_ack(delivery_tag=method.delivery_tag)
+    channel.basic_ack(delivery_tag=method.delivery_tag)
 
-    # response = {fib_result: fib(n)}
-    # json.dumps(response))
-
-channel.basic_qos(prefetch_count=1)
-channel.basic_consume(on_request, queue='rpc_queue')
-
-print(" [x] Awaiting Model Inference RPC requests")
-channel.start_consuming()
+publish_back()
